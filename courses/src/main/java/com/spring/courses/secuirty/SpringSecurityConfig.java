17efb51d46569e520cec182626cdc41833bc3a67
Karthikeya <karthi_keya@live.com>
2022-06-01 14:32:26 +0530

    User and Admin login

--- courses/src/main/java/com/spring/courses/secuirty/SpringSecurityConfig.java
+++ courses/src/main/java/com/spring/courses/secuirty/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,14 +20,23 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
+		/*
+		 * http.authorizeRequests().antMatchers("/swagger-ui/**",
+		 * "/student/**").permitAll().anyRequest().authenticated() .and().httpBasic();
+		 */
 
-		http.authorizeRequests().antMatchers("/swagger-ui/**", "/student/**").permitAll().anyRequest().authenticated()
-		.and().httpBasic()
+		http
 				// HTTP Basic authentication
-				.and().authorizeRequests().antMatchers(HttpMethod.GET, "/student/**").hasRole("USER")
+				.httpBasic().and().authorizeRequests().antMatchers(HttpMethod.GET, "/student/**").hasRole("USER")
 				.antMatchers(HttpMethod.POST, "/student").hasRole("ADMIN").and().csrf().disable().formLogin().disable();
 	}
-
+	
+	/*
+	 * @Override public void configure(WebSecurity web) throws Exception {
+	 * web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
+	 * "/swagger-resources/**", "/configuration/security", "/swagger-ui.html",
+	 * "/webjars/**"); }
+	 */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
